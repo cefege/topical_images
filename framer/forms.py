1407b7a1e01d0051445593283e409c1e58927066
iamlgndryAK <aadilkhan085529@gmail.com>
2024-08-05 14:43:22 +0530

    Delete framer directory

--- framer/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ImageUploadForm(forms.Form):
-    frame_image = forms.ImageField(label='Frame Image', required=False)
-    pool_image = forms.ImageField(label='Pool Image', required=False)
-    title = forms.CharField(max_length=255, required=False)
-    input_string = forms.CharField(widget=forms.Textarea, required=False)
-
--- framer/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class ImageUpload(models.Model):
-    frame_image = models.ImageField(upload_to='images/')
-    pool_image = models.ImageField(upload_to='images/')
