ba522120268d7cd314bd0149f9ba7918dc039a1c
iamlgndryAK <aadilkhan085529@gmail.com>
2024-08-05 14:36:21 +0530

    Delete framer directory

--- framer/templatetags/base64encode.py
+++
@@ -1,8 +0,0 @@
-import base64
-from django import template
-
-register = template.Library()
-
-@register.filter(name='b64encode')
-def b64encode(value):
-    return base64.b64encode(value).decode('utf-8')
